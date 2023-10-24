61c5cfcc2b4636dbcf4f68e9e431ca076c598d1f
LautareteX <carlosfrancoart@gmail.com>
2023-10-24 18:58:30 +0000

    num 3

--- models/base_model.py
+++ models/base_model.py
@@ -1,36 +1,31 @@
 #!/usr/bin/python3
 """BaseModel defines all common attributes/method"""
 from uuid import uuid4
-from datatime import datatime 
+from datetime import datetime
 
 
 
 class BaseModel:
     """class BaseModel"""
     def __init__(self, *args, **kwargs):
-    self.id = str(uuid4())
-    self.create_at = datatime.now()
-    self.updated_at = datatime.now()
-
+        self.id = str(uuid4())
+        self.create_at = datetime.now()
+        self.updated_at = self.create_at
 
     def save(self):
         """updates the public instance attribute"""
-        self.updated_at = datatime.now()
-        
+        self.updated_at = datetime.now()
+
     def to_dict(self):
         """returns a dictionary containing all keys/values of __dict__""""
-        return __dict__
-    
-        
+        return self.__dict__
 
+    now = datetime.now()
 
-
-
-
+    iso_str = now.isoformat()
 
     def __str__(self):
         """
         Returns a string representation of the rectangle.
         """
-        return f"[<class name>] ({self.id}) <self.__dict__>)
-    
+        return f"[{class name}]({self.id}) {self.__dict__})
