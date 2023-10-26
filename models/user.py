4c21baaf001a4075d5997fa172aecb4a1a0177ab
root <carlosfrancoart@gmail.com>
2023-10-26 18:20:16 +0000

    user

--- models/user.py
+++ models/user.py
@@ -1,6 +1,15 @@
 #!/usr/bin/python3
 """This is the User class"""
+from models.base_model import BaseModel
 
 
-class User():
-    pass
+class User(BaseModel):
+    """A new class User inheriting BaseModel"""
+    email = ""
+    password = ""
+    first_name = ""
+    last_name = ""
+
+    def __init__(self, *args, **kwargs):
+        """Creates new instances of User"""
+        super().__init__(*args, **kwargs)
