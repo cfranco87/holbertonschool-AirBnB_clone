be3159dd661079a464a61421472d22d3e2dde0cb
root <carlosfrancoart@gmail.com>
2023-10-28 20:27:30 +0000

    state

--- models/state.py
+++ models/state.py
@@ -2,5 +2,11 @@
 """This is the User class"""
 
 
-class State():
-    pass
+class State(BaseModel):
+    """creating state class"""
+
+    name = ""
+
+    def __init__def __init__(self, *args, **kwargs):
+        """Creates new instances of state."""
+        super().__init__(*args, **kwargs)
