f6660ac580cd2c53154db4935aee42f6bd1e2e97
root <carlosfrancoart@gmail.com>
2023-10-28 20:26:49 +0000

    review

--- models/review.py
+++ models/review.py
@@ -1,6 +1,14 @@
 #!/usr/bin/python3
-"""This is the User class"""
+"""This is the Review class inherit from BaseModel"""
+from models.base_model import BaseModel
 
 
-class Review():
-    pass
+class Review(BaseMode):
+    """creates Review class"""
+    place_id = ""
+    user_id = ""
+    text = ""
+
+    def __init__def __init__(self, *args, **kwargs):
+        """Creates new instances of City."""
+        super().__init__(*args, **kwargs)
