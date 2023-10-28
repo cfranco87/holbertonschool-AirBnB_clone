87fd4d98e2e748e6f57f506db40876817c3ef3cb
root <carlosfrancoart@gmail.com>
2023-10-28 20:20:44 +0000

    place

--- models/place.py
+++ models/place.py
@@ -1,6 +1,22 @@
 #!/usr/bin/python3
-"""This is the User class"""
+"""This is the Place class inherit from BaseModel"""
+from models.base_model import BaseModel
 
 
-class Place():
-    pass
+class Place(BaseModel):
+    """creates Place class"""
+    city_id = ""
+    user_id = ""
+    name = ""
+    description = ""
+    number_rooms = 0
+    number_bathrooms = 0
+    max_guest = 0
+    price_by_night = 0
+    latitude = 0.0
+    longitude = 0.0
+    amenity_ids = []
+
+    def __init__def __init__(self, *args, **kwargs):
+        """Creates new instances of City."""
+        super().__init__(*args, **kwargs)
