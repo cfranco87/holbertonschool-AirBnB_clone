aef62e8a614136bd95993a7ff53d5513cbd41a96
LautareteX <lautarorodriguezcoitinho@gmail.com>
2023-10-29 14:35:41 -0700

    trying to fix

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,12 +1,23 @@
 #!/usr/bin/python3
 """This module defines a class to manage file storage for hbnb clone"""
 import json
+from models.user import User
+from models.place import Place
+from models.state import State
+from models.city import City
+from models.amenity import Amenity
+from models.review import Review
 
 
 class FileStorage:
     """This class manages storage of hbnb models in JSON format"""
     __file_path = 'file.json'
     __objects = {}
+    __classes = {
+            'User': User, 'Place': Place,
+            'State': State, 'City': City, 'Amenity': Amenity,
+            'Review': Review
+    }
 
     def all(self):
         """Returns a dictionary representation of models"""
@@ -14,37 +25,26 @@ class FileStorage:
 
     def new(self, obj):
         """Adds new object to storage dictionary"""
-        self.all().update({obj.to_dict()['__class__'] + '.' + obj.id: obj})
+        key = obj.__class__.__name__ + "." + obj.id
+        FileStorage.__objects[key] = obj
 
     def save(self):
         """Saves storage dictionary to file"""
-        with open(FileStorage.__file_path, 'w') as f:
-            temp = {}
-            temp.update(FileStorage.__objects)
-            for key, val in temp.items():
-                temp[key] = val.to_dict()
-            json.dump(temp, f)
+        save_it = {}
+        for k, v in FileStorage.__objects.items():
+            save_it[k] = v.to_dict()
+
+        with open(FileStorage.__file_path, mode='w', encoding="utf-8") as f:
+            json.dump(save_it, f)
 
     def reload(self):
         """Loads storage dictionary from file"""
         from models.base_model import BaseModel
-        from models.user import User
-        from models.place import Place
-        from models.state import State
-        from models.city import City
-        from models.amenity import Amenity
-        from models.review import Review
 
-        classes = {
-                    'BaseModel': BaseModel, 'User': User, 'Place': Place,
-                    'State': State, 'City': City, 'Amenity': Amenity,
-                    'Review': Review
-                  }
         try:
-            temp = {}
-            with open(FileStorage.__file_path, 'r') as f:
-                temp = json.load(f)
-                for key, val in temp.items():
-                    self.all()[key] = classes[val['__class__']](**val)
+            with open(FileStorage.__file_path, mode="r") as my_file:
+                objects = json.load(my_file)
+                for key, value in objects.items():
+                    self.__objects[key] = BaseModel(**value)
         except FileNotFoundError:
             pass
